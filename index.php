d61e16e8c4bd490b02d402a22a1fce99e106d13a
Torikku-senpai <patrickabiog11@gmail.com>
2021-06-28 00:55:57 +0800

    Array

---
+++ index.php
@@ -0,0 +1,17 @@
+<center>
+<?php
+echo "Patrick Mathew L. Abiog";
+echo "<br/>";
+echo "BSIT 1";
+echo "<br/>";
+?></center>
+
+<?php
+$array = array(
+    1    => "a",
+    "1"  => "b",
+    1.5  => "c",
+    true => "d",
+);
+var_dump($array);
+?>
